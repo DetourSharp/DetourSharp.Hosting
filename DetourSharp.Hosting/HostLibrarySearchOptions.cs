3b981e003141d73476a74d9f67d8334b22a8cdee
DaZombieKiller <bennyboy.private@hotmail.com.au>
2021-11-26 18:54:31 +1000

    Remove Windows-only constructor in HostLibrarySearchOptions

--- DetourSharp.Hosting/HostLibrarySearchOptions.cs
+++ DetourSharp.Hosting/HostLibrarySearchOptions.cs
@@ -1,8 +1,4 @@
-﻿using System.Diagnostics;
-using System.Runtime.Versioning;
-using System.Runtime.InteropServices;
-using TerraFX.Interop.Windows;
-using static DetourSharp.Hosting.Windows;
+﻿using System.Runtime.InteropServices;
 namespace DetourSharp.Hosting;
 
 /// <summary>Provides configuration options for <see cref="HostLibrary.TryGetLibraryPath(HostLibrarySearchOptions, out string)"/>.</summary>
@@ -24,16 +20,6 @@ public sealed class HostLibrarySearchOptions
     public HostLibrarySearchOptions()
     {
         Architecture = RuntimeInformation.ProcessArchitecture;
-    }
-
-    /// <summary>Initializes a new <see cref="HostLibrarySearchOptions"/> instance with appropriate defaults for the given process.</summary>
-    [SupportedOSPlatform("windows")]
-    public HostLibrarySearchOptions(Process process)
-    {
-        if (Is64BitProcess((HANDLE)process.Handle))
-            Architecture = Architecture.X64;
-        else
-            Architecture = Architecture.X86;
     }
 
     /// <summary>Initializes a new <see cref="HostLibrarySearchOptions"/> instance with the values of another.</summary>
